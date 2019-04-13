5ed0f4d2763a17008eba97a535623dd244b1ea01
wangXing2 <1154505362@qq.com>
2019-04-13 20:20:51 +0800

    第一次提交

---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"errors"
+)
+
+type BitMap struct {
+	bits []byte
+	cap int64
+	oneCount int64
+}
+func NewBitMap(cap int64)*BitMap{
+	bm:=&BitMap{}
+	bm.bits=make([]byte,cap/8+1)
+	bm.cap=cap
+	return bm
+}
+func (bm *BitMap)Set(x int64)error{
+	div:=x/8
+	mod:=byte(x%8)
+	flag:=byte(1<<mod)
+	if x>bm.cap{
+		return errors.New("value is more than bitmap cap")
+	}
+	bm.bits[div]|=flag
+	return nil
+}
+func (bm *BitMap)SetN(x int64)(bool,error){
+	div:=x/8
+	mod:=byte(x%8)
+	flag:=byte(1<<mod)
+	if x>bm.cap{
+		return false,errors.New("value is more than bitmap cap")
+	}
+	v:=bm.bits[div]
+	v&=flag
+	if v>0{
+		return false,nil
+	}
+	bm.bits[div]|=flag
+	bm.oneCount++
+	return true,nil
+}
+func (bm *BitMap)ResetSetN(x int64)(bool,error){
+	div:=x/8
+	mod:=byte(x%8)
+	flag:=byte(1<<mod)
+	if x>bm.cap{
+		return false,errors.New("value is more than bitmap cap")
+	}
+	v:=bm.bits[div]
+	v&=flag
+	if v==0{
+		return false,nil
+	}
+	bm.bits[div]&=^flag
+	bm.oneCount--
+	return true,nil
+}
+func (bm *BitMap)Get(x int64)(bool,error){
+	div:=x/8
+	mod:=byte(x%8)
+	flag:=byte(1<<mod)
+	if x>bm.cap{
+		return false,errors.New("value is more than bitmap cap")
+	}
+	v:=bm.bits[div]
+	v&=flag
+	if v>0{
+		return true,nil
+	}else{
+		return false,nil
+	}
+
+}
+func (bm *BitMap)Len()int64{
+	return bm.oneCount
+}
---
+++ main_test.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestBitMap_Get(t *testing.T) {
+	bm:=NewBitMap(8)
+	err:=bm.Set(1)
+	if err!=nil{
+		t.Fatal(err)
+	}
+	res,err:=bm.Get(1)
+	if err!=nil{
+		t.Fatal(err)
+	}
+	if !res{
+		t.Fatal("该位没有置1")
+	}
+}
+func TestBitMap_SetN(t *testing.T) {
+	bm:=NewBitMap(8)
+	flag,err:=bm.SetN(1)
+	if err!=nil{
+		t.Fatal(err)
+	}
+	if flag==false{
+		t.Fatal("该位应该还没有设置，但是返回已经设置为1")
+	}
+	flag,err=bm.SetN(1)
+	if err!=nil{
+		t.Fatal(err)
+	}
+	if flag==true{
+		t.Fatal("该位应该已经设置了，但是返回没有设置")
+	}
+	res,err:=bm.Get(1)
+	if err!=nil{
+		t.Fatal(err)
+	}
+	if !res{
+		t.Fatal("该位应该已经设置，但是返回没有置1")
+	}
+}
+func TestBitMap_ResetSetN(t *testing.T) {
+	bm:=NewBitMap(8)
+	flag,err:=bm.SetN(1)
+	if err!=nil{
+		t.Fatal(err)
+	}
+	if flag==false{
+		t.Fatal("该位应该还没有设置，但是返回已经设置为1")
+	}
+	flag,err=bm.ResetSetN(1)
+	if err!=nil{
+		t.Fatal(err)
+	}
+	if flag==false{
+		t.Fatal("该位应该已经设置为0，但是返回没有设置")
+	}
+
+	flag,err=bm.ResetSetN(1)
+	if err!=nil{
+		t.Fatal(err)
+	}
+	if flag==true{
+		t.Fatal("该位应该已经设置了0，但是返回没有设置")
+	}
+
+	res,err:=bm.Get(1)
+	if err!=nil{
+		t.Fatal(err)
+	}
+	if res{
+		t.Fatal("该位应该已经设置0，但是返回没有置")
+	}
+}
+func TestBitMap_Overflow(t *testing.T) {
+	bm:=NewBitMap(8)
+	err:=bm.Set(9)
+	if err==nil{
+		t.Fatal("应该越界，但是返回正常")
+	}
+
+}
+
